d396320cfa69812ea626a76cdd5a9dabdda75b94
dsaecab0509 <dsaecab0509@g.educaand.es>
2024-09-27 18:33:13 +0200

    ampliacion

--- vamos.java
+++ vamos.java
@@ -1,6 +1,6 @@
 public class Vamos {
   public static void main(String[] args) {
     System.out.println("Madridista el que no bote");
-    System.out.println("Vini lloron")
+    System.out.println("Vini lloron asqueroso")
   }
 }
